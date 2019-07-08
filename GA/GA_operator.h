100906e63feb00890be83986813f228ffc1e7110
fawcat0906 <fawcat0906@gmail.com>
2019-07-08 17:14:35 +0800

    Divide vector-erase to GA_operator

--- GA/GA_operator.h
+++ GA/GA_operator.h
@@ -4,18 +4,49 @@ void selection()
 {
 }
 
+void vectorErase(int r1, int r2, vec_1D& targetVecotr)
+{
+    int valueToErase_1 = targetVecotr[r1];
+    int valueToErase_2 = targetVecotr[r2];
+    
+    vec_1D::iterator it = targetVecotr.begin();
+
+    for (; it != targetVecotr.end();)
+    {
+        if (*it == valueToErase_1 || *it == valueToErase_2)
+            it = targetVecotr.erase(it);
+        else
+            ++it;
+    }
+}
+
 void crossover(Chromosome &chromosomeList)
 {
-    float probabilityToCrossover = (double)rand() / (RAND_MAX);
     vec_1D remainChromosome(chromosomeNum);
-
-    /*初始化未crossover的chromosome*/
     for (int i = 0; i < chromosomeNum; i++)
         remainChromosome[i] = i;
 
-    for(int i = 0; i < (chromosomeNum / 2); i++)
+    /*初始化未crossover的chromosome*/
+
+    for (int i = 0; i < (chromosomeNum / 2); i++)
     {
-        
+        int r_1 = rand() % (remainChromosome.size());
+        int r_2 = rand() % (remainChromosome.size());
+        while (r_2 == r_1)
+            r_2 = rand() % (remainChromosome.size());
+
+        float probabilityToCrossover = (double)rand() / (RAND_MAX);
+
+        if (probabilityToCrossover < crossoverRate)
+        {
+        }
+
+        else
+            vectorErase(r_1,r_2,remainChromosome);
+
+        int cuttingPoint_1, cuttingPoint_2;
+        cuttingPoint_1 = rand() / solutionSize;
+        cuttingPoint_2 = rand() / solutionSize;
     }
     /*if (probabilityToCrossover < crossoverRate)
     {
